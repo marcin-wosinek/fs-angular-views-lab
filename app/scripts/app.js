82b93a5fa6d3671379a328dca9549d33acead886
Marcin Wosinek <marcin.wosinek@gmail.com>
2015-11-24 15:59:52 +0000

    generate code with yo

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name fsAngularViewsLabApp
+ * @description
+ * # fsAngularViewsLabApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('fsAngularViewsLabApp', []);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name fsAngularViewsLabApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the fsAngularViewsLabApp
+ */
+angular.module('fsAngularViewsLabApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
