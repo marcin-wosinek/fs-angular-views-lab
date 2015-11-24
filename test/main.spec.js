b8e3bec2d8c19297b3d6d37716a6ca609dac7b84
Marcin Wosinek <marcin.wosinek@gmail.com>
2015-11-24 17:12:57 +0000

    add a testing setup

---
+++ test/main.spec.js
@@ -0,0 +1,7 @@
+'use strict';
+
+describe('Main page', function() {
+  it('should show error on submitting wrong credentials', function() {
+
+  });
+});
---
+++ test/protractor.conf.js
@@ -0,0 +1,26 @@
+exports.config = {
+  // The address of a running selenium server.
+  seleniumAddress: 'http://localhost:4444/wd/hub',
+
+  // Spec patterns are relative to the location of this config.
+  specs: [
+    '*.spec.js'
+  ],
+
+  capabilities: {
+    browserName: 'chrome'
+  },
+
+  // A base URL for your application under test. Calls to protractor.get()
+  // with relative paths will be prepended with this.
+  baseUrl: 'http://localhost:9000/',
+
+  jasmineNodeOpts: {
+    onComplete: null,
+    isVerbose: false,
+    showColors: true,
+    includeStackTrace: true
+  },
+
+  framework: 'jasmine2'
+};
