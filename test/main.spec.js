ee05af048436b6b0f70ae14c0bb3059ff975a0df
Marcin Wosinek <marcin.wosinek@gmail.com>
2015-11-24 17:56:37 +0000

    cover the if with test

--- test/main.spec.js
+++ test/main.spec.js
@@ -1,7 +1,17 @@
 'use strict';
 
 describe('Main page', function() {
-  it('should show error on submitting wrong credentials', function() {
+  beforeEach(function() {
+    browser.get('#/main');
+  });
 
+  it('show hide message when animal provided', function() {
+    var animal = element(by.model('animal')),
+      message = element(by.id('message'));
+
+    expect(message.getText()).toEqual(jasmine.any(String));
+
+    animal.sendKeys('test');
+    expect(message.getText()).toEqual('');
   });
 });
